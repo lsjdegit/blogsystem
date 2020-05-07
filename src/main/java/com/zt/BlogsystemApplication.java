5146e43a6e0c20f6ab9f3d0e3b84c174cb11235d
lsj <2807728147@qq.com>
2020-05-07 16:35:48 +0800

    Initial commit

---
+++ src/main/java/com/zt/BlogsystemApplication.java
@@ -0,0 +1,13 @@
+package com.zt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsystemApplication.class, args);
+    }
+
+}
