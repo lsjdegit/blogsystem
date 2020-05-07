5ff83203df684da53730627209d47bdd45cad2db
lsj <2807728147@qq.com>
2020-05-07 19:29:26 +0800

    lsj

--- src/main/java/com/zt/entity/User.java
+++ src/main/java/com/zt/entity/User.java
@@ -17,6 +17,8 @@ public class User implements Serializable {
 	private String upassword;
 	private String uimage;
 	private Integer isadmin;
-
+	private String sex;
+	private String email;
+	private Integer age;
 
 }
