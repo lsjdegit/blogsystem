0a840ca0d4008a34088606ae4455909ab0afd308
lsj <2807728147@qq.com>
2020-05-12 09:37:10 +0800

    lsj

--- src/main/java/com/zt/BlogsystemApplication.java
+++ src/main/java/com/zt/BlogsystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.zt.mapper")
+@MapperScan(basePackages = "com.zt.mapper")
 public class BlogsystemApplication {
 
     public static void main(String[] args) {
