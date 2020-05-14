888972d10297c136f18d2374e9636a100480570c
lsj <2807728147@qq.com>
2020-05-14 19:51:14 +0800

    lsj

--- src/main/resources/static/js/news.js
+++ src/main/resources/static/js/news.js
@@ -1,16 +1,20 @@
- $(function(){
-			 	//首个变色		
-			$(".ul1 li:eq(0)").addClass("li");
-			//悬浮变色
-			$(".ul1 li").hover(function() {
-				$(".ul1 li").removeClass("dle");
-				$(this).addClass("dle");
-			}, function() {
-				$(".ul1 li").removeClass("dle");
-			})
+$(function () {
 
-			$(".ul1 li").click(function() {
-				$(".ul1 li").removeClass("li");
-				$(this).addClass("li");
-			})
-			 })
+    //头列表样式
+    $("#head-ul li:eq(3)").addClass("clickli");
+
+    //首个变色
+    $(".ul1 li:eq(0)").addClass("li");
+    //悬浮变色
+    $(".ul1 li").hover(function () {
+        $(".ul1 li").removeClass("dle");
+        $(this).addClass("dle");
+    }, function () {
+        $(".ul1 li").removeClass("dle");
+    })
+
+    $(".ul1 li").click(function () {
+        $(".ul1 li").removeClass("li");
+        $(this).addClass("li");
+    })
+})
